80fcf6bc1711e10e4bb60ffbbc6734bcd1a42f91
imen aouadi <imen.c.aouadi@gmail.com>
2020-12-20 22:16:54 +0000

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeartIcon from '../assets/heart.svg';
+import SocialIcon from './social_icon';
+
+const Footer = ({ social }) => (
+  <footer className="py-6 mx-auto items-center justify-between md:flex">
+   
+  
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Summary = ({ data }) => (
+  <section className="py-5 border-b border-neutral-300 lg:flex items-center">
+   
+    <p className="text-center tracking-wide leading-relaxed lg:text-left lg:mx-8 lg:text-lg">
+      {data}
+    </p>
+  </section>
+);
+
+export default Summary;
